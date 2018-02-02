43a9a4832a6bdb50cd3547200987efccb98b40c4
Daniel Echeverry Giraldo <dfecheverryg@unal.edu.co>
2018-02-02 05:40:22 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,10 @@ def oxford_comma(array)
 
   if array.size == 1
     array.join
-  elsif 
-     array.size == 2
+  elsif array.size == 2
+    last_array = array.pop
+    message = array.join(", ")
+    message << " and #{last_array}"
   else
     last_array = array.pop
     message = array.join(", ")
